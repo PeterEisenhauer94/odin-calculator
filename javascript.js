3ad8cde19a9ebced58d900ef3bca2879e18539ed
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-12-02 11:42:57 -0500

    improve button display storage

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 //global vars
 let calcDisplay = document.querySelector('#display-number');
-let num1 = 0;
-let num2 = '';
+let num = 0;
 
 //functions
 function add(num1,num2){
@@ -34,99 +33,99 @@ function updateDisplay(num){
 let btn1 = document.querySelector('#btn1');
 btn1.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='1';
-        updateDisplay(num2);
+        num='1';
+        updateDisplay(num);
     } else {
-        num2+='1';
-        updateDisplay(num2);
+        num+='1';
+        updateDisplay(num);
     };
 });
 
 let btn2 = document.querySelector('#btn2');
 btn2.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='2';
-        updateDisplay(num2);
+        num='2';
+        updateDisplay(num);
     } else {
-        num2+='2';
-        updateDisplay(num2);
+        num+='2';
+        updateDisplay(num);
     };
 });
 
 let btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='3';
-        updateDisplay(num2);
+        num='3';
+        updateDisplay(num);
     } else {
-        num2+='3';
-        updateDisplay(num2);
+        num+='3';
+        updateDisplay(num);
     };
 });
 
 let btn4 = document.querySelector('#btn4');
 btn4.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='4';
-        updateDisplay(num2);
+        num='4';
+        updateDisplay(num);
     } else {
-        num2+='4';
-        updateDisplay(num2);
+        num+='4';
+        updateDisplay(num);
     };
 });
 
 let btn5 = document.querySelector('#btn5');
 btn5.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='5';
-        updateDisplay(num2);
+        num='5';
+        updateDisplay(num);
     } else {
-        num2+='5';
-        updateDisplay(num2);
+        num+='5';
+        updateDisplay(num);
     };
 });
 
 let btn6 = document.querySelector('#btn6');
 btn6.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='6';
-        updateDisplay(num2);
+        num='6';
+        updateDisplay(num);
     } else {
-        num2+='6';
-        updateDisplay(num2);
+        num+='6';
+        updateDisplay(num);
     };
 });
 
 let btn7 = document.querySelector('#btn7');
 btn7.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='7';
-        updateDisplay(num2);
+        num='7';
+        updateDisplay(num);
     } else {
-        num2+='7';
-        updateDisplay(num2);
+        num+='7';
+        updateDisplay(num);
     };
 });
 
 let btn8 = document.querySelector('#btn8');
 btn8.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='8';
-        updateDisplay(num2);
+        num='8';
+        updateDisplay(num);
     } else {
-        num2+='8';
-        updateDisplay(num2);
+        num+='8';
+        updateDisplay(num);
     };
 });
 
 let btn9 = document.querySelector('#btn9');
 btn9.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
-        num2='9';
-        updateDisplay(num2);
+        num='9';
+        updateDisplay(num);
     } else {
-        num2+='9';
-        updateDisplay(num2);
+        num+='9';
+        updateDisplay(num);
     };
 });
 
@@ -135,8 +134,8 @@ btn0.addEventListener('click',()=>{
     if(calcDisplay.textContent=='0'){
         return;
     } else {
-        num2+='0';
-        updateDisplay(num2);
+        num+='0';
+        updateDisplay(num);
     }
 });
 
